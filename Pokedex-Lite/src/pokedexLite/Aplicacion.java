46b5a9f95d7e9fabb85af1c9f57efbb7b746faeb
Cristian2301 <cris.eze.99@gmail.com>
2021-05-11 12:18:55 -0300

    agregarPokemon funcionando

--- Pokedex-Lite/src/pokedexLite/Aplicacion.java
+++ Pokedex-Lite/src/pokedexLite/Aplicacion.java
@@ -27,38 +27,40 @@ public class Aplicacion {
 		System.out.println("Ingrese los datos del pokemon que desea agregar:");
 		
 		System.out.println("Nombre:");
-		String nombre = sc.nextLine();
+		String nombre = sc.next();
 		if (this.existePokemon(nombre)) {
 			throw new ArithmeticException("El pokemon ingresado ya existe");
 		}
 		
 		System.out.println("nivel:");
 		Integer nivel = sc.nextInt();
+
 		Pokemon pokemon = new Pokemon(nombre, nivel);
 
 		Integer opcion = 1;
 		String tipo;
-/*		while (opcion == 1) {
+		while (opcion == 1) {
 			System.out.println("Tipo:");
-			tipo = sc.nextLine();
+			tipo = sc.next();
 			pokemon.agregarTipo(tipo);
-			System.out.println("Si desea agregar otro tipo presione 1, sino presione 2");
+			System.out.println("Si desea agregar otro tipo presione 1, sino presione 2:");
 			opcion = sc.nextInt();
-		}*/
+		}
+		this.getPokemons().add(pokemon);
 		
-		Boolean ingresarTipos = true;
+/*		Boolean ingresarTipos = true;
 		while (ingresarTipos) {
 			System.out.println("Si desea agregar otro tipo presione 1, sino presione 2");
 			opcion = sc.nextInt();
 			if (opcion == 1) {
 				System.out.println("Tipo:");
-				tipo = sc.nextLine();
+				tipo = sc.next();
 				pokemon.agregarTipo(tipo);
 			}
 			else {
 				ingresarTipos = false;
 			}
-		}
+		}*/
 	}
 			
 		
@@ -73,14 +75,14 @@ public class Aplicacion {
 	public Boolean existePokemon(String nombre) {
 		Boolean existePokemon = false;
 		for (Pokemon p: this.getPokemons()) {
-			existePokemon = existePokemon || p.getNombre() == nombre;
+			existePokemon = existePokemon || p.getNombre().equals(nombre);
 		}
 		return existePokemon;
 	}	
 
 	public Pokemon buscarPokemon(String nombre) {
 		Integer i = 0;
-		while(this.pokemons.get(i).getNombre() != nombre) {
+		while(!(this.pokemons.get(i).getNombre().equals(nombre))) {
 			i++;
 		}
 		return pokemons.get(i);
